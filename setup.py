81f49f7bdbc7c4b613fb12537305030589a684b3
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-08-25 11:48:19 -0300

    Add sentiwordnet to setup

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import nltk
 if __name__ == "__main__":
     nltk.download('averaged_perceptron_tagger')
     nltk.download('punkt')
+    nltk.download('sentiwordnet')
     nltk.download('stopwords')
     nltk.download('wordnet')
